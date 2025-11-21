72a31581b67cc9b28a1147c91a2c0ec29566d83e
Krishan Dwivedi <krishandwive9@gmail.com>
2025-11-21 17:44:05 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,9 @@
 import type { Metadata } from "next";
-import {
-  DM_Serif_Display,
-  DM_Sans,
-  Libertinus_Serif,
-  Bungee,
-  Orbitron,
-  Abril_Fatface,
-} from "next/font/google";
+import { Bebas_Neue } from "next/font/google";
 import "./globals.css";
 
-const dmSerifDisplay = DM_Serif_Display({
-  variable: "--font-dm-serif-display",
-  subsets: ["latin"],
-  weight: ["400"],
-});
-
-const dmSans = DM_Sans({
-  variable: "--font-dm-sans",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
-
-const libertinusSerif = Libertinus_Serif({
-  variable: "--font-libertinus-serif",
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-});
-
-const bungee = Bungee({
-  variable: "--font-bungee",
-  subsets: ["latin"],
-  weight: ["400"],
-});
-
-const orbitron = Orbitron({
-  variable: "--font-orbitron",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
-
-const abrilFatface = Abril_Fatface({
-  variable: "--font-abril-fatface",
+const bebasNeue = Bebas_Neue({
+  variable: "--font-bebas-neue",
   subsets: ["latin"],
   weight: ["400"],
 });
@@ -59,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" style={{ maxWidth: "100vw", overflowX: "hidden" }}>
       <body
-        className={`${dmSerifDisplay.variable} ${dmSans.variable} ${libertinusSerif.variable} ${bungee.variable} ${orbitron.variable} ${abrilFatface.variable} antialiased`}
+        className={`${bebasNeue.variable} antialiased`}
         style={{ maxWidth: "100vw", overflowX: "hidden" }}
       >
         {children}
